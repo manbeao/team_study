56bae297fb5da8c51118386e857957d264e4021e
LEE EUN SEO <lesles0906@naver.com>
2024-09-04 14:23:12 +0900

    2

---
+++ src/main/java/24-09-03/operator_one.java
@@ -0,0 +1,10 @@
+public class operator_one {
+
+    public static void main(String[] args) {
+
+        /* 연산자를 이용하여 5와 3라는 숫자 로 값이 6이 나올수 있도록 구하시오 */
+        int num1 = 3;
+        int num2 = 5;
+        System.out.println("결과는 : " + ((num1 + num2)-(num2 - num1)) + "이다");
+    }
+}
